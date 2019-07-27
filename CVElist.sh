2726b23ac742e87bfb18b035ba620c3dbc9c8d41
苦力怕控 <2956226491@qq.com>
2019-07-28 02:29:05 +0800

    Update CVElist.sh

--- CVElist.sh
+++ CVElist.sh
@@ -12,7 +12,11 @@ wget http://cve.mitre.org/data/downloads/allitems.csv.gz
 wget http://cve.mitre.org/data/downloads/allitems.html.gz
 wget http://cve.mitre.org/data/downloads/allitems.txt.gz
 wget http://cve.mitre.org/data/downloads/allitems.xml.gz
-wget http://cve.mitre.org/data/downloads/allitems.csv
-wget http://cve.mitre.org/data/downloads/allitems.html
-wget http://cve.mitre.org/data/downloads/allitems.txt
-wget http://cve.mitre.org/data/downloads/allitems.xml
+#wget http://cve.mitre.org/data/downloads/allitems.csv
+#wget http://cve.mitre.org/data/downloads/allitems.html
+#wget http://cve.mitre.org/data/downloads/allitems.txt
+#wget http://cve.mitre.org/data/downloads/allitems.xml
+gzip -dk allitems.csv.gz
+gzip -dk allitems.html.gz
+gzip -dk allitems.txt.gz
+gzip -dk allitems.xml.gz

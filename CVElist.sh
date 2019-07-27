705d92bd7f7aa5ae32da4a46c52cf1ca850a2e25
苦力怕控 <2956226491@qq.com>
2019-07-28 02:18:06 +0800

    Create CVElist.sh

---
+++ CVElist.sh
@@ -0,0 +1,15 @@
+vDate=`date +%Y%m%d`
+cd /data/wwwroot/mirrors/CVE/list
+mkdir $vDate
+wget http://cve.mitre.org/data/downloads/allitems.csv.Z
+wget http://cve.mitre.org/data/downloads/allitems.html.Z
+wget http://cve.mitre.org/data/downloads/allitems.txt.Z
+wget http://cve.mitre.org/data/downloads/allitems.xml.Z
+wget http://cve.mitre.org/data/downloads/allitems.csv.gz
+wget http://cve.mitre.org/data/downloads/allitems.html.gz
+wget http://cve.mitre.org/data/downloads/allitems.txt.gz
+wget http://cve.mitre.org/data/downloads/allitems.xml.gz
+wget http://cve.mitre.org/data/downloads/allitems.csv
+wget http://cve.mitre.org/data/downloads/allitems.html
+wget http://cve.mitre.org/data/downloads/allitems.txt
+wget http://cve.mitre.org/data/downloads/allitems.xml

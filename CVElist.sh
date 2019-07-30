98a1bb0f8944619f3e614522abfd3af84b82e84d
苦力怕控 <2956226491@qq.com>
2019-07-30 19:13:30 +0800

    Update CVElist.sh

--- CVElist.sh
+++ CVElist.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
+# Set Date
 vDate=`date +%Y%m%d`
+
+# Goto Directory
 cd /data/wwwroot/mirrors/CVE/list
+
+# Clean existing content
 rm -f *
+
+# Create data of update
 echo $vDate | tee updated.txt
+
+# Download original
 wget http://cve.mitre.org/data/downloads/allitems.csv.Z
 wget http://cve.mitre.org/data/downloads/allitems.html.Z
 wget http://cve.mitre.org/data/downloads/allitems.txt.Z
 wget http://cve.mitre.org/data/downloads/allitems.xml.Z
-wget http://cve.mitre.org/data/downloads/allitems.csv.gz
-wget http://cve.mitre.org/data/downloads/allitems.html.gz
-wget http://cve.mitre.org/data/downloads/allitems.txt.gz
-wget http://cve.mitre.org/data/downloads/allitems.xml.gz
+#wget http://cve.mitre.org/data/downloads/allitems.csv.gz
+#wget http://cve.mitre.org/data/downloads/allitems.html.gz
+#wget http://cve.mitre.org/data/downloads/allitems.txt.gz
+#wget http://cve.mitre.org/data/downloads/allitems.xml.gz
 #wget http://cve.mitre.org/data/downloads/allitems.csv
-#wget http://cve.mitre.org/data/downloads/allitems.html
+#curl -o allitems.html http://cve.mitre.org/data/downloads/allitems.html
 #wget http://cve.mitre.org/data/downloads/allitems.txt
 #wget http://cve.mitre.org/data/downloads/allitems.xml
-gzip -cd allitems.csv.gz > allitems.csv
-gzip -cd allitems.html.gz > allitems.html
-gzip -cd allitems.txt.gz > allitems.txt
-gzip -cd allitems.xml.gz > allitems.xml
+
+# Extract *.Z
+7za x allitems.csv.Z
+7za x allitems.html.Z
+7za x allitems.txt.Z
+7za x allitems.xml.Z
+
+# Compress by gzip
+gzip -c9 allitems.csv > allitems.csv.gz
+gzip -c9 allitems.html > allitems.html.gz
+gzip -c9 allitems.txt > allitems.txt.gz
+gzip -c9 allitems.xml > allitems.xml.gz
